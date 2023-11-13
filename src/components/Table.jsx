5cfd22c0d31756b9aebb6c5e53897505fe90c5cb
Cameron Flowers <cameron.flowers13@gmail.com>
2023-11-13 12:40:24 -0500

    adding table body support

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -11,12 +11,25 @@ function TableHeader(){
     )
 }
 
-function TableBody(){
-/*responsible for rendering the data for our table */
-    return(
-        <tbody></tbody>
-    )
-}
+const TableBody = (props) => {
+    // boilerplate table body functional component
+    // we use Array.map to create table rows from LinkData passed via props
+    const rows = props.linkData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>
+            <a href={row.URL}>{row.URL}</a>
+          </td>
+          <td>
+            <button onClick={() => props.removeLink(index)}>Delete</button>
+          </td>
+        </tr>
+      )
+    })
+  
+    return <tbody>{rows}</tbody>
+  }
 
 
 function Table(){
