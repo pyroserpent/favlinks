638bd0f59fb1ef3aef70a4f0943646782459d1c7
pyroserpent <zacafnan@gmail.com>
2023-12-11 22:42:20 -0500

    Updated client to fetch API for CRUD

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,29 +6,32 @@ function TableHeader() {
             <tr>
                 <th>Name</th>
                 <th>URL</th>
-                <th>Remove</th>
+                <th>Actions</th>
             </tr>
         </thead>
     );
 }
 
-const TableBody = ({ linkData, removeLink }) => {
+const TableBody = ({ linkData, removeLink, editLink }) => {
     const rows = linkData.map((row, index) => (
         <tr key={index}>
             <td>{row.name}</td>
             <td><a href={row.URL}>{row.URL}</a></td>
-            <td><button onClick={() => removeLink(index)}>Delete</button></td>
+            <td>
+                <button onClick={() => editLink(index)}>Edit</button>
+                <button onClick={() => removeLink(index)}>Delete</button>
+            </td>
         </tr>
     ));
 
     return <tbody>{rows}</tbody>;
 };
 
-function Table({ linkData, removeLink }) {
+function Table({ linkData, removeLink, editLink }) {
     return (
         <table>
             <TableHeader />
-            <TableBody linkData={linkData} removeLink={removeLink} />
+            <TableBody linkData={linkData} removeLink={removeLink} editLink={editLink} />
         </table>
     );
 }
